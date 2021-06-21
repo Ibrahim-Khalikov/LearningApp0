ec8ab849df21768d7583255cdc25df7d0ab2e2b7
Ibrahim Khalikov <starlink050@gmail.com>
2021-06-21 08:17:16 +0500

    Initial Commit

---
+++ LearningApp0/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LearningApp0
+//
+//  Created by cloud_vfx on 21/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LearningApp0/LearningApp0App.swift
@@ -0,0 +1,17 @@
+//
+//  LearningApp0App.swift
+//  LearningApp0
+//
+//  Created by cloud_vfx on 21/06/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningApp0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
